5618de1ccce495052765ad4ffd61ca267eae4333
Alexmarpar <188632620+alexmarpar@users.noreply.github.com>
2026-06-22 03:14:02 +0200

    Reformat backend/main.py with no functional changes

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["POST"],
-    allow_headers=["Content-Type""],
+    allow_headers=["Content-Type"],
 )
 
 # Routers
